69ce07ec4e44da6f0929c11ade09d421dcbfe7f6
aceLeVillegas <sarahvillegas95@gmail.com>
2018-02-02 17:10:51 -0800

    Set up API call and parsing of the data.

--- Flix/Flix/NowPlayingViewController.swift
+++ Flix/Flix/NowPlayingViewController.swift
@@ -17,16 +17,21 @@ class NowPlayingViewController: UIViewController {
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
-            // This will run when the network request returns
-            if let error = error {
+            // runs when the network request returns
+            if let error = error{
                 print(error.localizedDescription)
-            } else if let data = data {
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            }
+            else if let data = data {
                 
-                // TODO: Get the array of movies
-                // TODO: Store the movies in a property to use elsewhere
-                // TODO: Reload your table view data
+               let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 
+                // Parsing through data to get titles or movies
+                let movies = dataDictionary["results"] as! [[String:Any]]
+                for movie in movies{
+                    let title = movie["title"] as! String
+                
+                    print(title)
+                }
             }
         }
         task.resume()
