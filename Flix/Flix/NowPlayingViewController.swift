4e1150c939555312a108ed230c9cb4323660ba15
aceLeVillegas <sarahvillegas95@gmail.com>
2018-02-02 19:40:00 -0800

    Set up a demo view of a cell

--- Flix/Flix/NowPlayingViewController.swift
+++ Flix/Flix/NowPlayingViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var MovieTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        MovieTableView.dataSource = self
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -24,17 +29,28 @@ class NowPlayingViewController: UIViewController {
             else if let data = data {
                 
                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                
+                print(dataDictionary)
                 // Parsing through data to get titles or movies
                 let movies = dataDictionary["results"] as! [[String:Any]]
                 for movie in movies{
                     let title = movie["title"] as! String
-                
-                    print(title)
+                    let description = movie["overview"] as! String
+                    let posterImg = movie["poster_path"] as! String
                 }
             }
         }
         task.resume()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = MovieTableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
